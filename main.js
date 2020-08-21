3d739786e3c68c6ca99735c474db3f4ebbea978a
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2020-08-21 22:07:02 +0900

    p152/set up db conection with the server locally

--- main.js
+++ main.js
@@ -3,11 +3,32 @@ const express = require('express'),
   homeController = require('./controllers/homeController'),
   errorController = require('./controllers/errorController');
 
+// Load Mongodb module
+const MongoDB = require('mongodb').MongoClient,
+  dbURL = "mongodb://localhost:27017",
+  dbName = "recipe_db";
+
+// set up db connection locally
+MongoDB.connect(dbURL, {
+  useUnifiedTopology: true
+}, (error, client) => {
+  if (error) throw error;
+  // get recipe_db by Mongodb server
+  let db = client.db(dbName);
+  // get all records from contacts collection
+  db.collection("contacts")
+    .find()
+    .toArray((error, data) => {
+      if (error) throw error;
+      console.log(data);
+    });
+});
+
 // load express-ejs-layouts, set the layout module to the app
 const layout = require('express-ejs-layouts');
 app.set("view engine", "ejs");
 app.use(layout);
-Æ
+
 app.set("port", process.env.PORT || 3000);
 app.use(express.static("public"));
 
