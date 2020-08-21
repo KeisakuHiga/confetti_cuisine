b319299074e6b00580b8e37a6b5b568c7b8da61e
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2020-08-21 22:19:40 +0900

    p153 / finish lesson13: connecting Mongodb

--- main.js
+++ main.js
@@ -1,42 +1,60 @@
 const express = require('express'),
-  app = express(),
-  homeController = require('./controllers/homeController'),
-  errorController = require('./controllers/errorController');
+	app = express(),
+	homeController = require('./controllers/homeController'),
+	errorController = require('./controllers/errorController');
 
 // Load Mongodb module
 const MongoDB = require('mongodb').MongoClient,
-  dbURL = "mongodb://localhost:27017",
-  dbName = "recipe_db";
+	dbURL = 'mongodb://localhost:27017',
+	dbName = 'recipe_db';
 
 // set up db connection locally
-MongoDB.connect(dbURL, {
-  useUnifiedTopology: true
-}, (error, client) => {
-  if (error) throw error;
-  // get recipe_db by Mongodb server
-  let db = client.db(dbName);
-  // get all records from contacts collection
-  db.collection("contacts")
-    .find()
-    .toArray((error, data) => {
-      if (error) throw error;
-      console.log(data);
-    });
-});
+MongoDB.connect(
+	dbURL,
+	{
+		useUnifiedTopology: true,
+	},
+	(error, client) => {
+		if (error) throw error;
+		// get recipe_db by Mongodb server
+		let db = client.db(dbName);
+
+		// insert a new item
+		db.collection('contacts').insert(
+			{
+				name: 'Hiroto',
+				age: 37,
+				note: 'Big brother',
+			},
+			(error, item) => {
+				if (error) throw error;
+				console.log(item);
+			},
+		);
+
+		// get all records from contacts collection
+		db.collection('contacts')
+			.find()
+			.toArray((error, data) => {
+				if (error) throw error;
+				console.log(data);
+			});
+	},
+);
 
 // load express-ejs-layouts, set the layout module to the app
 const layout = require('express-ejs-layouts');
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 app.use(layout);
 
-app.set("port", process.env.PORT || 3000);
-app.use(express.static("public"));
+app.set('port', process.env.PORT || 3000);
+app.use(express.static('public'));
 
 // middleware
 app.use(
-  express.urlencoded({
-    extended: false
-  })
+	express.urlencoded({
+		extended: false,
+	}),
 );
 app.use(express.json());
 
@@ -51,6 +69,6 @@ app.use(errorController.pageNotFoundError);
 app.use(errorController.internalServerError);
 
 // listening port
-app.listen(app.get("port"), () => {
-  console.log(`Server running at http://localhost:${app.get("port")}`)
+app.listen(app.get('port'), () => {
+	console.log(`Server running at http://localhost:${app.get('port')}`);
 });
