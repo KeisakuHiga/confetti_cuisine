530ffd4e57a84f2cf0372afcb2606591f0aed929
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2020-08-22 09:15:38 +0900

    p159 create a new model using Subscriber schema and save it to db

--- main.js
+++ main.js
@@ -27,8 +27,36 @@ const subscriberSchema = mongoose.Schema({
 // adapt the above schema to Model, 1st argument: model name, 2nd argument: the defined schema
 const Subscriber = mongoose.model("Subscriber", subscriberSchema);
 
+// create a new Subscriber object - part1 using 'new' and 'save' method
+var subscriber1 = new Subscriber({ // use 'new'
+  name: "iPhone 7",
+  email: "i7@apple.com"
+});
+subscriber1.save((error, savedDoc) => { // use 'save'
+  if (error) console.log(error);
+  console.log(savedDoc);
+});
+
+// create a new Subscriber object - part2 using 'create' method
+Subscriber.create({
+    name: "Michel Jordan",
+    email: "mj@mj.com"
+  },
+  (error, savedDoc) => {
+    if (error) console.log(error);
+    console.log(savedDoc);
+  }
+);
+
+// get all documents from Subscriber
+Subscriber.find({}, (error, allDocs) => {
+  if (error) console.log(error.stack);
+  console.log("Here is all documents:\n", allDocs);
+})
+
 // load express-ejs-layouts, set the layout module to the app
 const layout = require('express-ejs-layouts');
+const e = require('express');
 app.set('view engine', 'ejs');
 app.use(layout);
 
