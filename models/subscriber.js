45d244b707526f25c2d81063d6119ace8e4db2b8
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2020-08-22 15:32:49 +0900

    p187 changed subscriberSchema adding validations for each property

--- models/subscriber.js
+++ models/subscriber.js
@@ -3,9 +3,21 @@ const mongoose = require('mongoose');
 
 // create a new schema of subscribers
 const subscriberSchema = mongoose.Schema({
-  name: String, // add schema property
-  email: String,
-  zipCode: Number
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: true
+  },
+  zipCode: {
+    type: Number,
+    min: [1000000, 'Zip code too short'],
+    max: 9999999
+  }
 });
 // adapt the above schema to Model, 1st argument: model name, 2nd argument: the defined schema
 module.exports = mongoose.model("Subscriber", subscriberSchema);
