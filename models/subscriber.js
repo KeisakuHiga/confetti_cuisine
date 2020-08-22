beaccf0658bd98e882384aff52b2bdc235a6bcdb
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2020-08-22 15:49:59 +0900

    p188 added instance methods to subscriberSchema

--- models/subscriber.js
+++ models/subscriber.js
@@ -19,5 +19,20 @@ const subscriberSchema = mongoose.Schema({
     max: 9999999
   }
 });
+
+// add instance methods to subscriberSchema
+// this method returns info about a subscriber
+subscriberSchema.methods.getInfo = function () {
+  return `Name: ${this.name} Email: ${this.email} Zip Code: ${this.zipCode}`;
+};
+// this method finds the subscribers who have the same zip code
+subscriberSchema.methods.findLocalSubscribers = function () {
+  return this.model("Subscriber")
+    .find({
+      zipCode: this.zipCode
+    })
+    .exec(); // access Subscriber model to use find method
+};
+
 // adapt the above schema to Model, 1st argument: model name, 2nd argument: the defined schema
 module.exports = mongoose.model("Subscriber", subscriberSchema);
