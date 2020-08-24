74b92139330076c5024ceddea214d709ea7044d3
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2020-08-24 23:07:22 +0900

    p209  modified errorController and subscribersController for being able to exports all controller actions by object literal like homeController

--- controllers/subscribersController.js
+++ controllers/subscribersController.js
@@ -1,42 +1,44 @@
 // load subscriber module
 const Subscriber = require('../models/subscriber');
 
-// get all subscribers data 
-exports.getAllSubscribers = (req, res) => {
-  Subscriber.find({})
-    .exec()
-    .then(result => {
-      res.render("subscribers", {
-        subscribers: result
+module.exports = {
+  // get all subscribers data 
+  getAllSubscribers: (req, res) => {
+    Subscriber.find({})
+      .exec()
+      .then(result => {
+        res.render("subscribers", {
+          subscribers: result
+        });
+      })
+      .catch(error => {
+        console.log(error.message);
+        return [];
+      })
+      .then(() => {
+        console.log('promise complete');
       });
-    })
-    .catch(error => {
-      console.log(error.message);
-      return [];
-    })
-    .then(() => {
-      console.log('promise complete');
+  },
+  // rendering contact page
+  getSubscriptionPage: (req, res) => {
+    res.render("contact")
+  },
+  // save posted subscribers' data from client form 
+  saveSubscriber: (req, res) => {
+    // create a new Subscriber object
+    let newSubscriber = new Subscriber({
+      name: req.body.name,
+      email: req.body.email,
+      zipCode: req.body.zipCode
     });
-};
-// rendering contact page
-exports.getSubscriptionPage = (req, res) => {
-  res.render("contact")
-};
-// save posted subscribers' data from client form 
-exports.saveSubscriber = (req, res) => {
-  // create a new Subscriber object
-  let newSubscriber = new Subscriber({
-    name: req.body.name,
-    email: req.body.email,
-    zipCode: req.body.zipCode
-  });
 
-  newSubscriber.save()
-    .then(result => {
-      console.log(`Saved new subscriber: \n${result}`)
-      res.render("thanks");
-    })
-    .catch(error => {
-      if (error) res.send(error);
-    });
-};
+    newSubscriber.save()
+      .then(result => {
+        console.log(`Saved new subscriber: \n${result}`)
+        res.render("thanks");
+      })
+      .catch(error => {
+        if (error) res.send(error);
+      });
+  }
+}
