4367961ae1527f207333b16d3122e8bd09a2ea0d
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2020-08-24 23:57:24 +0900

    p213 modified usersController to split 'query' action and 'rendering' action

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,16 +1,20 @@
 const User = require('../models/user');
 
 module.exports = {
-  index: (req, res) => {
+  index: (req, res, next) => {
     User.find({})
       .then(users => {
-        res.render("users/index", {
-          users: users
-        })
+        // save users data to response object and call middleware
+        res.locals.users = users;
+        next()
       })
       .catch(error => {
         console.log(`Error fetching users: #${error.message}`)
-        res.redirect("/");
+        next(error);
       });
+  },
+  // rendering by other action
+  indexView: (req, res) => {
+    res.render("users/index");
   }
 }
