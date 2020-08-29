782ee3c8beb64b7a0bedbc34a22ed68a8420812e
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2020-08-29 12:28:13 +0900

    p266 updated other controllers as usersContorller

--- controllers/subscribersController.js
+++ controllers/subscribersController.js
@@ -35,13 +35,16 @@ module.exports = {
 		// console.log(subscriberParams)
 		Subscriber.create(subscriberParams)
 			.then((subscriber) => {
+				req.flash("success", `${subscriber.name} was added to our subscribed members successfully`);
 				res.locals.redirect = '/subscribers';
 				res.locals.subscriber = subscriber;
 				next();
 			})
 			.catch((error) => {
 				console.log(`Error saving a new subscriber: ${error.message}`);
-				next(error);
+				res.locals.redirect = "/subscribers/new";
+				req.flash("error", `Failed to create subscriber account because: ${error.message}.`);
+				next();
 			});
 	},
 	redirectView: (req, res, next) => {
@@ -89,31 +92,37 @@ module.exports = {
 			subscriberParams = getSubscriberParams(req.body);
 		// find a subscriber and update the subscribers' data
 		Subscriber.findByIdAndUpdate(subscriberId, {
-			$set: subscriberParams,
-		})
+				$set: subscriberParams,
+			})
 			.then((subscriber) => {
 				// console.log(`found subscriber: ${subscriber}`);
 				// add updated subscriber data to a local variable
-				res.locals.redirect = `/subscribers/${subscriberId}`;
+        req.flash("updated", `${subscriber.name} has been updated successfully`);
+        res.locals.redirect = `/subscribers/${subscriberId}`;
 				res.locals.subscriber = subscriber;
 				// call middleware
 				next();
 			})
 			.catch((error) => {
 				console.log(`Error updating subscriber by ID: ${error.message}`);
-				next(error);
+        res.locals.redirect = `/subscribers/${subscriberId}/edit`;
+        req.flash("error", `Failed to update subscriber because: ${error.message}.`);
+        next();
 			});
 	},
 	delete: (req, res, next) => {
 		let subscriberId = req.params.id;
 		Subscriber.findByIdAndDelete(subscriberId)
 			.then(() => {
-				res.locals.redirect = '/subscribers';
+        req.flash("deleted", `${subscriberId} has been deleted successfully`);
+        res.locals.redirect = '/subscribers';
 				next();
 			})
 			.catch((error) => {
 				console.log(`Error deleting subscriber by ID: ${error.message}`);
-				next(error);
+        res.locals.redirect = "/subscribers";
+        req.flash("error", `Failed to delete subscriber because: ${error.message}`);
+        next();
 			});
 	},
 };
