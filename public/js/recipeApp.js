c74d233351551bd7ca918d3dedb047522e1cc1f5
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2020-09-06 16:47:00 +0900

    p326-327 added event listener for each join button

--- public/js/recipeApp.js
+++ public/js/recipeApp.js
@@ -13,9 +13,34 @@ $(document).ready(() => {
               <div class="course-description">${course.description}</div>
               <button class="join-button" data-id="${course._id}">Join</button>
             </div>`
-          )
-        })
+          );
+        });
       }
+    }).then(() => {
+      // After finishing Ajax request, call addJoinButtonListener to add event listener for each button
+      addJoinButtonListener();
     });
   });
 });
+
+let addJoinButtonListener = () => {
+  $(".join-button").click(event => {
+    // confirm target button
+    let $button = $(event.target),
+      // get course id data
+      courseId = $button.data("id");
+    // Ajax request with course id you want to join
+    $.get(`/api/courses/${courseId}/join`, (results = {}) => {
+      let data = results.data;
+      // after confirming the request, update course data you joined
+      if (data && data.success) {
+        $button
+          .text("Joined!")
+          .addClass("joined-button")
+          .removeClass("join-button");
+      } else {
+        $button.text("Failed to join the course! Try again!");
+      }
+    });
+  });
+}
